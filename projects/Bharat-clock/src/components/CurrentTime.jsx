cbc9d625095b2ad8b387375360ba425674c35dca
const-nishant <nishantspatil04@gmail.com>
2024-09-25 19:51:59 +0530

    (day 40)Made chnages to the bharat clock project using new hooks learned

--- projects/Bharat-clock/src/components/CurrentTime.jsx
+++ projects/Bharat-clock/src/components/CurrentTime.jsx
@@ -1,7 +1,26 @@
-function CurrentTime() {
-  let time = new Date().toLocaleTimeString();
+import { useEffect, useState } from "react";
 
-  return <p className="lead text-center">This is the current time: {time}</p>;
+function CurrentTime() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("setting interval");
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      console.log("clearing interval");
+    };
+  }, []);
+
+  return (
+    <p className="lead text-center">
+      This is the current time: {time.toLocaleTimeString()} -{" "}
+      {time.toLocaleDateString()}
+    </p>
+  );
 }
 
 export default CurrentTime;
