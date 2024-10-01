3e4f085df81adcc66030b4c924ff50d43eed29da
const-nishant <nishantspatil04@gmail.com>
2024-10-01 21:22:58 +0530

    (day 47 ) working on the major project myntra clone  done with most of the UI part backend remaning

---
+++ projects/Myntra Clone/myntra-clone/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./routes/App.jsx";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/home.jsx";
+
+const router = createBrowserRouter([
+  {
+    Patth: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
