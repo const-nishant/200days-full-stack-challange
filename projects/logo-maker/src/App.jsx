2fee5545600224ffea378dfc4905bea7367f8046
const-nishant <nishantspatil04@gmail.com>
2024-10-05 22:14:12 +0530

    (day 51 ) working on the logo maker completed the icon controller logic

--- projects/logo-maker/src/App.jsx
+++ projects/logo-maker/src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+import BackgroundController from "./components/BackgroundController";
 import Header from "./components/Header";
+import IconController from "./components/IconController";
 import SideNav from "./components/SideNav";
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <>
       <Header />
       <div className="w-64 fixed">
-        <SideNav selectedIndex={() => console.log("selected")} />
+        <SideNav selectedIndex={(value) => setSelectedIndex(value)} />
       </div>
-      <div className="mx-64 grid grid-cols-1 md:grid-cols-6">
-        <div className="md:col-span-2 bg-green-200">control panel</div>
-        <div className="md:col-span-4 bg-red-100">icon preview</div>
+      <div className="mx-64 grid grid-cols-1 md:grid-cols-6 fixed">
+        <div className="md:col-span-4 border h-screen shadow-sm p-5 overflow-auto">
+          {selectedIndex === 0 ? <IconController /> : <BackgroundController />}
+        </div>
+        <div className="md:col-span-2">icon preview</div>
       </div>
     </>
   );
--- projects/logo-maker/src/components/Header.jsx
+++ projects/logo-maker/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Download } from "lucide-react";
 
 function Header() {
   return (
-    <div className="p-4  shadow-sm border flex items-center justify-between">
+    <div className="p-2  shadow-sm border flex items-center justify-between">
       <img src="/logo.svg" alt="logo" />
       <Button className="flex gap-2 items-center">
         <Download className="w-4 h-4" /> Download
